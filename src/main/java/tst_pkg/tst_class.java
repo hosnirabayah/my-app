093563214c8a7d3e325559ec0ca39f715586c261
Rabaia <hosni.rabaia@iqvia.com>
2024-10-21 09:53:28 +0300

    first commit from b2

--- src/main/java/tst_pkg/tst_class.java
+++ src/main/java/tst_pkg/tst_class.java
@@ -3,5 +3,7 @@ package tst_pkg;
 public class tst_class {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World from b2");
+        
     }
 }
